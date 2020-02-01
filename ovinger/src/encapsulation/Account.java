88b0b7921566d894a7a6976a16597ef2f17cb8ff
Jørgen Stamnes <jorghs@stud.ntnu.no>
2020-02-01 08:51:45 +0000

    Figuring out javafxml, added workspace full vnc

--- ovinger/src/encapsulation/Account.java
+++ ovinger/src/encapsulation/Account.java
@@ -1,7 +1,43 @@
 package encapsulation;
 
 class Account {
-    public static void main(String[] args) {
-        System.out.println("Hello this is account!");
+    private double balance;
+    private double interestRate;
+
+	public Account(final double balance, final double interestRate) {
+        this.balance = balance;
+        this.interestRate = interestRate;
+	}
+
+	public double getBalance() {
+		return this.balance;
     }
+
+	public double getInterestRate() {
+		return this.interestRate;
+	}
+
+	public void setInterestRate(final double interestRate) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deposit(final double amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void withdraw(final double amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void addInterest() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(final String[] args) {
+		System.out.println("Used for testing Account");
+	}
 }
--- ovinger/tests/encapsulation/AccountTest.java
+++ ovinger/tests/encapsulation/AccountTest.java
@@ -3,9 +3,7 @@ package encapsulation;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
