f19df5b48a26e5d1273faf73d1289ed6589e37cf
Jørgen Stamnes <jorghs@stud.ntnu.no>
2020-02-03 11:41:28 +0000

    Add the quickness of compiling the code yourself with javac + java

--- ovinger/src/encapsulation/account/AccountApp.java
+++ ovinger/src/encapsulation/account/AccountApp.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class AccountApp extends Application{
+public class AccountApp extends Application {
 
 	@Override
 	public void start(final Stage primaryStage) throws Exception {
