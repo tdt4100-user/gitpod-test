789f72fbc6093bc40e19338055a6b265c277ead2
Jørgen Stamnes <jorghs@stud.ntnu.no>
2020-02-20 09:29:45 +0000

    Add readme files for each and every task in øving1

--- ovinger/src/stateandbehavior/Digit.java
+++ ovinger/src/stateandbehavior/Digit.java
@@ -2,4 +2,13 @@ package stateandbehavior;
 
 public class Digit {
 
+    private int digit;
+
+    public Digit(int digit){
+        this.digit = digit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("My Digit class");
+    }
 }
